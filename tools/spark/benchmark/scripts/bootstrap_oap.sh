456b3d0cbad598e5763ed67e8ee7a56882c6c160
HongW2019 <hong2.wang@intel.com>
2022-04-27 09:31:33 +0800

    Add oap bootstrap

---
+++ tools/spark/benchmark/scripts/bootstrap_oap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install -y git
+export USER_HOME=/home/$(whoami)
+export BENCHMARK_TOOL_HOME=$USER_HOME/runtime/benchmark-tools
+mkdir -p "$BENCHMARK_TOOL_HOME"
+oap_install_dir=$BENCHMARK_TOOL_HOME/oap
+
+
+## Step 2: conda install oap
+
+which conda && conda create -p "${oap_install_dir}" -c conda-forge -c intel -y oap=1.3.1.dataproc20
