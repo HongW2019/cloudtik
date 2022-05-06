fd36ec471df2774507bb1ae1b964fbbe83cea6c1
HongW2019 <hong2.wang@intel.com>
2022-05-06 09:44:01 +0800

    Fix index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,14 @@
 # -- Project information
 project = 'CloudTik'
 
+html_theme_options = {
+    "repository_url": "https://github.com/oap-project/cloudtik.git",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "path_to_docs": "docs/source",
+    "home_page_in_toc": True,
+}
 
 # The suffix of source filenames.
 
