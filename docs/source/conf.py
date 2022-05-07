5452109f23bf5c638db0004eb0792ad593fc660f
HongW2019 <hong2.wang@intel.com>
2022-05-07 20:17:28 +0800

    Update the theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
     "repository_url": "https://github.com/oap-project/cloudtik.git",
