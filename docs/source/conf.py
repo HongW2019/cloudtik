5306da48529d6c09ad38b89c7d07da9f99d9f358
HongW2019 <hong2.wang@intel.com>
2022-05-11 13:31:37 +0800

    Add guides

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,15 +24,15 @@ author = 'CloudTik Developers'
 # The full version, including alpha/beta/rc tags
 release = '0.9.0'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['recommonmark',
-              'sphinx_markdown_tables'
-]
+              'sphinx_markdown_tables',
+              'sphinx_tabs.tabs'
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,14 +42,12 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
