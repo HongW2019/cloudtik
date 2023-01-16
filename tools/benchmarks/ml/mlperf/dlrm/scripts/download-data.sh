310c040c5348aa05283913486d5463affe20aad4
HongW2019 <hong2.wang@intel.com>
2023-01-16 18:53:02 +0800

    First commit

---
+++ tools/benchmarks/ml/mlperf/dlrm/scripts/download-data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# download Criteo Terabyte dataset
+# the dataset contains 24 zipped files and requires about 1 TB storage for the data and another 2 TB for immediate results
+
+curl -O -C - https://storage.googleapis.com/criteo-cail-datasets/day_{$(seq -s "," 0 23)}.gz
+yes n | gunzip -k day_{0..23}.gz
